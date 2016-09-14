81df7cb624cbc4a0dc6ae8ef7e6d8bf6fbb5a505
Mike Ihbe <mikejihbe@gmail.com>
2016-09-13 17:38:58 -0700

    Add network configuration to our truffle

--- eth/truffle.js
+++ eth/truffle.js
@@ -10,5 +10,18 @@ module.exports = {
   rpc: {
     host: "localhost",
     port: 8545
+  },
+  "live": {
+    network_id: 1, // Ethereum public network
+  },
+  "morden": {
+    network_id: 2,        // Official Ethereum test network
+  },
+  "staging": {
+    network_id: 1337 // custom private network
+    // use default rpc settings
+  },
+  "development": {
+    network_id: "default",
   }
 };
