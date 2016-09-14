9632e643d7f1f5a36e839d973182238cf453f091
John Weldon <johnweldon4@gmail.com>
2016-09-14 12:02:14 -0700

    parameterize rpc url and interval

--- oracles/src/oracles/cmd/blockwatcher/main.go
+++ oracles/src/oracles/cmd/blockwatcher/main.go
@@ -12,14 +12,31 @@ import (
 
 func main() {
 	app := cli.NewApp()
+	app.Usage = "Watch new blocks on an ethereum blockchain"
 	app.Action = Main
+	app.Version = "0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "rpcurl",
+			Value:  "http://127.0.0.1:8545",
+			Usage:  "url for RPC connection to ethereum node",
+			EnvVar: "ETH_RPCURL",
+		},
+		cli.IntFlag{
+			Name:  "interval",
+			Value: 15,
+			Usage: "polling interval in seconds",
+		},
+	}
 	app.Run(os.Args)
 }
 
 func Main(c *cli.Context) error {
-	bw := oracles.NewBlockWatcher("http://127.0.0.1:8545", 15*time.Second)
+	rpcurl := c.String("rpcurl")
+	interval := time.Duration(c.Int("interval"))
+	bw := oracles.NewBlockWatcher(rpcurl, interval*time.Second)
 	for id := range bw.Ch {
 		fmt.Printf("new block %q\n", id)
 	}
-	return nil
+	return bw.Wait()
 }
--- oracles/src/oracles/cmd/eventwatcher/main.go
+++ oracles/src/oracles/cmd/eventwatcher/main.go
@@ -12,18 +12,35 @@ import (
 
 func main() {
 	app := cli.NewApp()
+	app.Usage = "Watch events on an ethereum blockchain"
 	app.Action = Main
+	app.Version = "0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "rpcurl",
+			Value:  "http://127.0.0.1:8545",
+			Usage:  "url for RPC connection to ethereum node",
+			EnvVar: "ETH_RPCURL",
+		},
+		cli.IntFlag{
+			Name:  "interval",
+			Value: 15,
+			Usage: "polling interval in seconds",
+		},
+	}
 	app.Run(os.Args)
 }
 
 func Main(c *cli.Context) error {
+	rpcurl := c.String("rpcurl")
+	interval := time.Duration(c.Int("interval"))
 	r := &oracles.BlockRange{
 		From: "0x1",
 		To:   "latest",
 	}
-	evtWatcher := oracles.NewEventWatcher("http://127.0.0.1:8545", 15*time.Second, r, "")
+	evtWatcher := oracles.NewEventWatcher(rpcurl, interval*time.Second, r, "")
 	for evt := range evtWatcher.Ch {
 		fmt.Printf("new event %+v\n", evt)
 	}
-	return nil
+	return evtWatcher.Wait()
 }
