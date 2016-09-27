32466422621d0463bd1d35f6581c5d046784d1a7
John Weldon <johnweldon4@gmail.com>
2016-09-26 20:37:52 -0700

    Allow envars to override rpc connection details

--- eth/truffle.js
+++ eth/truffle.js
@@ -1,27 +1,27 @@
+var host = process.env.ETHEREUM_SERVICE_HOST || 'localhost';
+var port = process.env.ETHEREUM_SERVICE_PORT || 8545;
+
 module.exports = {
   build: {
     "index.html": "index.html",
-    "app.js": [
-    ],
-    "app.css": [
-    ],
+    "app.js": [],
+    "app.css": [],
     "images/": "images/"
   },
   rpc: {
-    host: "localhost",
-    port: 8545
+    host: host,
+    port: port
   },
   "live": {
     network_id: 1, // Ethereum public network
   },
   "morden": {
-    network_id: 2,        // Official Ethereum test network
+    network_id: 2, // Official Ethereum test network
   },
   "staging": {
     network_id: 1337 // custom private network
-    // use default rpc settings
   },
   "development": {
-    network_id: "default",
+    network_id: "default" // Development network
   }
 };
